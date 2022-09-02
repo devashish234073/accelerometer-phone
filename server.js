cfa0a4a9eeb4bd60171d770a8345f5bf16aef5b5
Devashish Priyadarshi <devashish234073@gmail.com>
2022-09-02 14:22:22 +0530

    implemented data endpoint

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require("http");
 var fs = require("fs");
+const url = require('url');
+
+
 var server = http.createServer(serverfunction);
 var data = fs.readFileSync("accelerometer.html");
 function serverfunction(req,res) {
   if(req.url=="/") {
     res.end(data);
+  } else if(req.url.indexOf("/data")==0) {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+    res.end(JSON.stringify(queryObject));
   }
 }
 var PORT = 8080;
